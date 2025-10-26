2c669899b72fd6f1315f81974984141a65079a6c
Pranjal Shinde <shinde.pc1707@gmail.com>
2025-10-26 20:12:07 +0530

    TabBar Controller Added

--- Runnr/Track Screen/TrackScreenViewController.swift
+++ Runnr/Track Screen/TrackScreenViewController.swift
@@ -11,7 +11,7 @@ class TrackScreenViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        navigationItem.hidesBackButton = true
         // Do any additional setup after loading the view.
     }
 
---
+++ Runnr/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Runnr
+//
+//  Created by Pranjal Shinde on 16/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let tabBar = UITabBarController()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        let itemOne = ActivityScreenViewController()
+        let iconOne = UITabBarItem(title:NSLocalizedString("Activities", comment: ""), image: UIImage(systemName: "figure.run.square.stack.fill"), selectedImage: UIImage(systemName: "figure.run.square.stack.fill"))
+        itemOne.tabBarItem = iconOne
+        
+        let itemTwo = ClubScreenViewController()
+        let iconTwo = UITabBarItem(title:NSLocalizedString("Club", comment: ""), image: UIImage(systemName: "person.2.fill"), selectedImage: UIImage(systemName: "person.2.fill"))
+        itemTwo.tabBarItem = iconTwo
+
+        let itemThree = TrackScreenViewController()
+        let iconThree = UITabBarItem(title:NSLocalizedString("Track", comment: ""), image: UIImage(systemName: "figure.run"), selectedImage: UIImage(systemName: "figure.run"))
+        itemThree.tabBarItem = iconThree
+
+        let itemFour = GameScreenViewController()
+        let iconFour = UITabBarItem(title:NSLocalizedString("Game", comment: ""), image: UIImage(systemName: "gamecontroller.fill"), selectedImage: UIImage(systemName: "gamecontroller.fill"))
+        itemFour.tabBarItem = iconFour
+
+        let itemFive = StatisticsScreenViewController()
+        let iconFive = UITabBarItem(title:NSLocalizedString("Statistics", comment: ""), image: UIImage(systemName: "chart.bar.fill"), selectedImage: UIImage(systemName: "chart.bar.fill"))
+        itemFive.tabBarItem = iconFive
+        
+        let tabBarControllerArray = [itemOne, itemTwo, itemThree, itemFour, itemFive]
+        
+        tabBar.setViewControllers(tabBarControllerArray, animated: false)
+        tabBar.selectedIndex = 2
+        tabBar.modalPresentationStyle = .fullScreen
+        self.present(tabBar, animated: false)
+        
+    }
+
+}
+
