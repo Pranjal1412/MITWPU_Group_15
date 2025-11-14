91bdf0511c340d8249b229ffa553b965f9c2eb54
Pranjal Shinde <shinde.pc1707@gmail.com>
2025-11-14 23:23:19 +0530

    Run Started Screen - Update 2

--- Runnr/Track Screen/RunStartedViewController.swift
+++ Runnr/Track Screen/RunStartedViewController.swift
@@ -9,22 +9,60 @@ import UIKit
 
 class RunStartedViewController: UIViewController {
 
+    @IBOutlet weak var viewBackground: UIView!
+    @IBOutlet weak var viewTime: UIView!
+    @IBOutlet weak var viewPace: UIView!
+    @IBOutlet weak var viewHeartRate: UIView!
+    @IBOutlet weak var viewDistance: UIView!
+    @IBOutlet weak var buttonPause: UIButton!
+    
+    @IBOutlet weak var labelDistance: UILabel!
+    @IBOutlet weak var labelTime: UILabel!
+    @IBOutlet weak var labelPace: UILabel!
+    @IBOutlet weak var labelHeartRate: UILabel!
+    
+    @IBOutlet weak var labelTimeCounter: UILabel!
+    @IBOutlet weak var labelPaceCounter: UILabel!
+    @IBOutlet weak var labelHeartRateCounter: UILabel!
+    @IBOutlet weak var labelDistanceCounter: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.overrideUserInterfaceStyle = .dark
-        // Do any additional setup after loading the view.
+        settingCornerRadius()
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func settingCornerRadius() {
+        viewBackground.layer.cornerRadius = 20
+        viewPace.layer.cornerRadius = 20
+        viewHeartRate.layer.cornerRadius = 20
+        viewTime.layer.cornerRadius = 20
+        viewDistance.layer.cornerRadius = 20
+        buttonPause.layer.cornerRadius = 50
+        
+        labelDistance.font = UIFont(name: "SF Pro Medium", size: 18.0)
+        labelDistance.text = NSLocalizedString("Distance (Km)", comment: "")
+        
+        labelTime.font = UIFont(name: "SF Pro Medium", size: 18.0)
+        labelTime.text = NSLocalizedString("Time", comment: "")
+        
+        labelPace.font = UIFont(name: "SF Pro Medium", size: 18.0)
+        labelPace.text = NSLocalizedString("Pace", comment: "")
+        
+        labelHeartRate.font = UIFont(name: "SF Pro Medium", size: 18.0)
+        labelHeartRate.text = NSLocalizedString("Heart Rate", comment: "")
+        
+        labelPaceCounter.font = UIFont(name: "SF Pro Regular", size: 20)
+        labelTimeCounter.font = UIFont(name: "SF Pro Regular", size: 55)
+        labelDistanceCounter.font = UIFont(name: "SF Pro Regular", size: 128)
+        labelHeartRateCounter.font = UIFont(name: "SF Pro Regular", size: 20)
+        
     }
-    */
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+//        buttonPause.layer.cornerRadius = 50
+    }
+    
 }
