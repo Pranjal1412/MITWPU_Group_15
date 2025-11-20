c41bafe1160e5f24706df2a1d44bd08653b8204f
Pranjal_Shinde <shinde.pc1707@gmail.com>
2025-11-20 16:20:54 +0530

    My Activity Screen table view cell

---
+++ Runnr/Activity Screen/MyActivityTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell.swift
+//  Runnr
+//
+//  Created by Archit Kankaria on 18/11/25.
+//
+
+import UIKit
+
+class MyActivityTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
