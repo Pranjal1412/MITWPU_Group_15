589955d27d781afe52edc2c870ac8f58397b6d39
archit2005 <architkankaria2005@gmail.com>
2025-11-25 16:03:26 +0530

    table view implemented

--- Runnr/Activity Screen/ActivityScreenViewController.swift
+++ Runnr/Activity Screen/ActivityScreenViewController.swift
@@ -1,29 +1,32 @@
-//
-//  ActivityScreenViewController.swift
-//  Runnr
-//
-//  Created by Pranjal Shinde on 26/10/25.
-//
-
 import UIKit
 
-class ActivityScreenViewController: UIViewController {
+class ActivityScreenViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet weak var tableView: UITableView!
 
+    @IBOutlet weak var segmentedControlActivityScreen: UISegmentedControl!
+    // Example Data Model
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.overrideUserInterfaceStyle = .dark
+        segmentedControlActivityScreen.layer.borderWidth = 0.5
+            
+            segmentedControlActivityScreen.layer.borderColor = UIColor.accent.cgColor
+        segmentedControlActivityScreen.setTitleTextAttributes([.foregroundColor: UIColor.black], for: .selected)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "MyActivityTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
     }
 
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return activities.count
     }
-    */
 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! MyActivityTableViewCell
+        
+        let activity = activities[indexPath.row]
+        cell.configure(with: activity)
+        return cell
+    }
 }
