9da011a46c8519ecd285db6713485b6f336bc94f
Mrunal Aralkar <mrunalaralkar@gmail.com>
2025-12-29 18:15:40 +0530

    made small changes to calender

--- Runnr/Insights Screen/cardData.swift
+++ Runnr/Insights Screen/cardData.swift
@@ -18,10 +18,10 @@ var cardDataArray: [CardData] = [
              trend: "1.2 km more than last week", trendChevron: "chevron.up.2"),
 
     CardData(number: "528", unit: "Kcal", title: "Calories Burnt",
-             trend: "50 kcal higher", trendChevron: "chevron.down.2"),
+             trend: "50 kcal lower", trendChevron: "chevron.down.2"),
 
     CardData(number: "10.5", unit: "k", title: "Steps Covered",
-             trend: "1200 more less", trendChevron: "chevron.up.2"),
+             trend: "1200 more", trendChevron: "chevron.up.2"),
 
     CardData(number: "7:90", unit: "min/km", title: "Average Pace",
              trend: "2 minutes slower", trendChevron: "chevron.down.2")
