b6f68880cfc9d5278b8830688b58c2b0c0fc6b3e
archit2005 <architkankaria2005@gmail.com>
2026-01-09 00:54:54 +0530

    Changed views to buttons in the second game set screen

--- Runnr/Game Screen/GameSetTwoViewController.swift
+++ Runnr/Game Screen/GameSetTwoViewController.swift
@@ -9,13 +9,7 @@ import UIKit
 
 class GameSetTwoViewController: UIViewController {
 
-    
-    @IBOutlet weak var viewMint: UIView!
-    
-    @IBOutlet weak var viewCyan: UIView!
-    
-    @IBOutlet weak var viewRed: UIView!
-    
+    private var selectedColor: UIColor = .systemGreen
     override func viewDidLoad() {
         super.viewDidLoad()
         overrideUserInterfaceStyle = .dark
@@ -28,6 +22,21 @@ class GameSetTwoViewController: UIViewController {
         self.dismiss(animated: true, completion: nil)
         let destinationVC = BattleRunViewController()
         self.present(destinationVC, animated: true, completion: nil)
+    }
+    
+    @IBAction func buttonMint(_ sender: UIButton) {
+        selectedColor = UIColor(red: 0.68, green: 1.0, blue: 0.0, alpha: 1.0) // Mint/Lime
+                print("Mint selected")
+    }
+    
+    @IBAction func buttonCyan(_ sender: UIButton) {
+        selectedColor = .cyan
+                print("Cyan selected")
+    }
+    
+    @IBAction func buttonRed(_ sender: Any) {
+        selectedColor = .systemRed
+                print("Red selected")
     }
     
 }
