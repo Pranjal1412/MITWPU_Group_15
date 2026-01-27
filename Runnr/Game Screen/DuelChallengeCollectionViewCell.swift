81f4214b1aef7f326f2d25efdeb13d5421bdae10
Pranjal Shinde <shinde.pc1707@gmail.com>
2026-01-27 19:48:07 +0530

    game screen cells created

---
+++ Runnr/Game Screen/DuelChallengeCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DuelChallengeCollectionViewCell.swift
+//  Runnr
+//
+//  Created by Pranjal Shinde on 27/01/26.
+//
+
+import UIKit
+
+class DuelChallengeCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
