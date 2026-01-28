9b1e4b054b3bc7543628faabd4f6f8d06086e37f
Pranjal_Shinde <shinde.pc1707@gmail.com>
2026-01-28 16:04:10 +0530

    game screen solo and monthly event cells completed

--- Runnr/Game Screen/SoloChallengeCollectionViewCell.swift
+++ Runnr/Game Screen/SoloChallengeCollectionViewCell.swift
@@ -9,9 +9,35 @@ import UIKit
 
 class SoloChallengeCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var viewCellBackground: UIView!
+    @IBOutlet weak var imageViewChallenge: UIImageView!
+    @IBOutlet weak var labelChallengeHeading: UILabel!
+    @IBOutlet weak var labelRewardPoints: UILabel!
+    @IBOutlet weak var labelChallengeDescription: UILabel!
+    @IBOutlet weak var progressChallengeCompletion: UIProgressView!
+    @IBOutlet weak var labelCompletionPercent: UILabel!
+    @IBOutlet weak var labelCompletionNumber: UILabel!    
+    @IBOutlet weak var viewRewardPoints: UIView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
     }
 
+    func configureCell() {
+        self.viewCellBackground.layer.cornerRadius = 15
+        self.imageViewChallenge.image = UIImage(systemName: "sun.horizon.fill")
+        self.imageViewChallenge.layer.cornerRadius = self.imageViewChallenge.frame.height / 2
+        self.labelChallengeHeading.text = String(localized: "Sunrise Run")
+        self.labelRewardPoints.text = "+50"
+        self.viewRewardPoints.layer.cornerRadius = self.viewRewardPoints.frame.height / 2
+        self.viewRewardPoints.layer.borderWidth = 1
+        self.viewRewardPoints.layer.borderColor = UIColor.accent.cgColor
+        self.labelChallengeDescription.text = String(localized: "Goal: Run before 7 AM three days in a week")
+        self.progressChallengeCompletion.progress = 0.2
+        self.labelCompletionPercent.text = "20%"
+        self.labelCompletionNumber.text = "2.4/5.0 Km"
+        
+    }
+    
 }
