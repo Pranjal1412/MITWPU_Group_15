ad7cacb84b9ab269c133202ac0fa068565fb4997
Pranjal_Shinde <shinde.pc1707@gmail.com>
2026-03-05 17:23:16 +0530

    minor change

--- Runnr/Club Screen/ClubScreenViewController.swift
+++ Runnr/Club Screen/ClubScreenViewController.swift
@@ -81,15 +81,6 @@ class ClubScreenViewController: UIViewController {
             collectionViewJoinedClub.reloadData()
             collectionViewExplore.reloadData()
             buttonAddMoreClubs.isHidden = true
-            
-            segmentControlClubScreen.selectedSegmentIndex = 1
-            collectionViewExplore.isHidden = false
-            buttonCreateClub.isHidden = true
-            searchBarFriendsScreen.isHidden = false
-            labelCreateyourOwnClub.isHidden = true
-            tableViewFriends.isHidden = true
-            searchBarFriendsScreen.placeholder = "Search for clubs"
-            collectionViewJoinedClub.isHidden = true
 
         }
         
