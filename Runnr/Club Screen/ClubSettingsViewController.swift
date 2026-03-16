6717e6efd0e5fb515bfd23000bc6eb5a1a64b694
Pranjal_Shinde <shinde.pc1707@gmail.com>
2026-03-16 14:25:10 +0530

    club settings

--- Runnr/Club Screen/ClubSettingsViewController.swift
+++ Runnr/Club Screen/ClubSettingsViewController.swift
@@ -14,35 +14,26 @@ class ClubSettingsViewController: UIViewController {
     @IBOutlet var scrollView: UIScrollView!
     @IBOutlet weak var buttonSave: UIButton!
     @IBOutlet weak var buttonCancel: UIButton!
+    
+    @IBOutlet weak var imageClubProfile: UIImageView!
+    @IBOutlet weak var imageClubBanner: UIImageView!
+    
+    var clubProfileData: Club?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setup()
-        scrollView.contentSize.height = self.stackJoinApproval.frame.origin.y + self.stackJoinApproval.frame.height + 20
         
-        // Do any additional setup after loading the view.
     }
     
     func setup() {
-//        imageViewClubProfilePicture.layer.cornerRadius = imageViewClubProfilePicture.frame.size.height / 2
-////        buttonEditProfilePhoto.layer.cornerRadius = buttonEditProfilePhoto.frame.size.height / 2
-//        viewClubName.layer.cornerRadius = 10
-//        viewDescription.layer.cornerRadius = 10
-//        viewClubName.clipsToBounds = true
-//        viewClubPrivacy.layer.cornerRadius = 10
-//        viewClubPrivacy.clipsToBounds = true
-//        viewDescription.clipsToBounds = true
-//        
-//        viewClubName.layer.borderWidth = 1
-//        viewClubName.layer.borderColor = UIColor.cardLightBlack.cgColor
-//        viewDescription.layer.borderWidth = 1
-//        viewDescription.layer.borderColor = UIColor.cardLightBlack.cgColor
-//        viewClubPrivacy.layer.borderWidth = 1
-//        viewClubPrivacy.layer.borderColor = UIColor.cardLightBlack.cgColor
-        
+        scrollView.contentSize.height = self.stackJoinApproval.frame.origin.y + self.stackJoinApproval.frame.height + 20
         setGlassEffect(for: self.buttonCancel, withImage: "multiply")
         setGlassEffect(for: self.buttonSave, withImage: "checkmark")
     }
+    
+    
     
     @IBAction func buttonSave(_ sender: UIButton) {
     }
@@ -50,15 +41,4 @@ class ClubSettingsViewController: UIViewController {
     @IBAction func buttonCancel(_ sender: UIButton) {
         self.dismiss(animated: true)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
