8ba92af437e08f7214fe072b7458ca03b72364c5
Mrunal Aralkar <mrunalaralkar@gmail.com>
2026-03-27 12:57:52 +0530

    game description screen designed, add contraints, setup layout left

---
+++ Runnr/Game Screen/GameDescriptionViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameDescriptionViewController.swift
+//  Runnr
+//
+//  Created by Mrunal Aralkar on 27/03/26.
+//
+
+import UIKit
+
+class GameDescriptionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
