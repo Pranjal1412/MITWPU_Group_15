b62730f9afa4a2187132c99f41b541ecb53056d8
Mrunal Aralkar <mrunalaralkar@gmail.com>
2026-05-07 11:26:54 +0530

    event collection view cell changes

--- Runnr/Club Screen/EventCollectionViewCell.swift
+++ Runnr/Club Screen/EventCollectionViewCell.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class EventCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var buttonDate: UIButton!
+    @IBOutlet weak var buttonTime: UIButton!
+    @IBOutlet weak var labelDescriptionOfEvent: UILabel!
+    @IBOutlet weak var viewEventStartEnd: UIView!
+    @IBOutlet weak var labelStartFrom: UILabel!
+    @IBOutlet weak var labelDateAndTime: UILabel!
+    @IBOutlet weak var labelDateTime: UILabel!
+    @IBOutlet weak var buttonMaybe: UIButton!
+    @IBOutlet weak var buttonNo: UIButton!
+    @IBOutlet weak var buttonYes: UIButton!
+    @IBOutlet weak var labelAreYouComing: UILabel!
+    @IBOutlet weak var labelEndAt: UILabel!
+    @IBOutlet weak var labelSunriseRun: UILabel!
+    @IBOutlet weak var viewMain: UIView!
     override func awakeFromNib() {
         super.awakeFromNib()
+        setup()
         // Initialization code
+    }
+    func setup() {
+        viewMain.layer.cornerRadius = 15
     }
 
 }
