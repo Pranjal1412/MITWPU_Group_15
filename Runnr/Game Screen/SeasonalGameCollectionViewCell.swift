a207a703a29b763f6ea37d5c8726cf3bfbf0c23e
archit2005 <architkankaria2005@gmail.com>
2026-05-23 09:46:08 +0530

    game date changed

--- Runnr/Game Screen/SeasonalGameCollectionViewCell.swift
+++ Runnr/Game Screen/SeasonalGameCollectionViewCell.swift
@@ -115,7 +115,7 @@ class SeasonalGameCollectionViewCell: UICollectionViewCell {
         let now = Date()
         let components = calendar.dateComponents([.day, .month, .year], from: now)
 
-        if let day = components.day, day >= 1 && day <= 25 {
+        if let day = components.day, day >= 1 && day <= 15 {
             return (true, nil)
         } else {
             var nextMonthComponents = DateComponents()
